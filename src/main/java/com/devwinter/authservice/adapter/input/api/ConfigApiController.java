9d23e390dc7f7ebc64e98eabfc5452ef17a7f990
Lee Dong Hun <109887384+donghun93@users.noreply.github.com>
2023-02-23 18:31:15 +0900

    Feature/#7 config server init (#9)
    * Feat: auth-service와 config server 연동
    
    
    Resolvers: #7
    
    * Build: Config server 연동 테스트 컨트롤러 수정

--- src/main/java/com/devwinter/authservice/adapter/input/api/ConfigApiController.java
+++ src/main/java/com/devwinter/authservice/adapter/input/api/ConfigApiController.java
@@ -1,16 +1,20 @@
 package com.devwinter.authservice.adapter.input.api;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class ConfigApiController {
     @Value("${auth.version}")
     private String version;
 
+    private final Environment env;
+
     @GetMapping("/api/v1/auth/version")
     public String version() {
-        return version;
-    }
+        return "@Value: " + version + ", Environment: " + env.getProperty("auth.version");
 }
