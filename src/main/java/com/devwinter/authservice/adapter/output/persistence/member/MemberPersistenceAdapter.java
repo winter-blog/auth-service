4e069971cfc78e43a8345efe7a925ba73746e940
donghun93 <alswn4516@gmail.com>
2023-03-08 14:19:59 +0900

    Feact: 회원 정보 캐싱 위치 변경
    서비스 계층에서 회원 정보 캐싱처리를 Adapter 계층으로 변경
    
    Resolvers: #21

--- src/main/java/com/devwinter/authservice/adapter/output/persistence/member/MemberPersistenceAdapter.java
+++ src/main/java/com/devwinter/authservice/adapter/output/persistence/member/MemberPersistenceAdapter.java
@@ -3,11 +3,11 @@ package com.devwinter.authservice.adapter.output.persistence.member;
 import com.devwinter.authservice.application.exception.AuthErrorCode;
 import com.devwinter.authservice.application.exception.AuthException;
 import com.devwinter.authservice.application.port.output.LoadMemberPort;
+import com.devwinter.authservice.common.CacheKey;
 import com.devwinter.authservice.domain.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 import static com.devwinter.authservice.application.exception.AuthErrorCode.MEMBER_ALREADY_DELETE;
 
@@ -18,8 +18,8 @@ public class MemberPersistenceAdapter implements LoadMemberPort {
     private final MemberJpaEntityRepository memberRepository;
     private final MemberMapper memberMapper;
 
-
     @Override
+    @Cacheable(value = CacheKey.MEMBER, key = "#email", unless = "#result == null")
     public Member findByEmail(String email) {
         MemberJpaEntity memberJpaEntity = memberRepository.findByEmail(email)
                                                           .orElseThrow(() -> new AuthException(AuthErrorCode.MEMBER_NOT_FOUND));
@@ -27,16 +27,8 @@ public class MemberPersistenceAdapter implements LoadMemberPort {
         return memberMapper.entityToDomain(memberJpaEntity);
     }
 
-    @Override
-    public Member findById(Long id) {
-        MemberJpaEntity memberJpaEntity = memberRepository.findById(id)
-                                                          .orElseThrow(() -> new AuthException(AuthErrorCode.MEMBER_NOT_FOUND));
-        memberDeleteValid(memberJpaEntity);
-        return memberMapper.entityToDomain(memberJpaEntity);
-    }
-
     private void memberDeleteValid(MemberJpaEntity memberJpaEntity) {
-        if(memberJpaEntity != null && memberJpaEntity.isDeleted()) {
+        if (memberJpaEntity != null && memberJpaEntity.isDeleted()) {
             throw new AuthException(MEMBER_ALREADY_DELETE);
         }
     }
