e7424eb12731eefd1d4133588c10240b74a20f5d
donghun93 <alswn4516@gmail.com>
2023-03-08 11:00:43 +0900

    Feat: 회원 인증 캐싱 처리
    로그인과 회원 인증 부분에 대해서 캐싱 처리함
    로그인은 @CachePut을 사용하여 AuthMemberDto의 값을 Redis에 저장
    회원 인증 부분은 @Cacheable을 사용하여 Redis에 AuthMemberDto가 있는 경우 해당 값 읽고 없으면 DB에 접근
    
    Resolvers: #17

--- src/main/java/com/devwinter/authservice/application/service/GetMemberValidService.java
+++ src/main/java/com/devwinter/authservice/application/service/GetMemberValidService.java
@@ -1,9 +1,11 @@
 package com.devwinter.authservice.application.service;
 
+import com.devwinter.authservice.application.dto.AuthMemberDto;
 import com.devwinter.authservice.application.port.input.GetMemberValidQuery;
 import com.devwinter.authservice.application.port.output.LoadMemberPort;
 import com.devwinter.authservice.domain.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +14,9 @@ public class GetMemberValidService implements GetMemberValidQuery {
     private final LoadMemberPort loadMemberPort;
 
     @Override
-    public ValidMemberDto valid(Long memberId) {
+    @Cacheable(value = "MEMBER", key = "#memberId", cacheManager = "cacheManager")
+    public AuthMemberDto valid(Long memberId) {
         Member member = loadMemberPort.findById(memberId);
-        return ValidMemberDto.of(member);
+        return AuthMemberDto.of(member);
     }
 }
