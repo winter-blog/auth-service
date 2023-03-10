15f282c8bd8e6344ab514ec7c8368e831ff3c59d
donghun93 <alswn4516@gmail.com>
2023-03-10 15:17:13 +0900

    Feat: 회원 비밀번호 변경 Domain Event 생성 및 Consumer 개발
    Resolvers: #23

---
+++ src/main/java/com/devwinter/authservice/adapter/input/message/kafka/PasswordChangeEventConsumer.java
@@ -0,0 +1,30 @@
+package com.devwinter.authservice.adapter.input.message.kafka;
+
+import com.devwinter.authservice.application.port.input.LogoutMemberUseCase;
+import com.devwinter.authservice.domain.event.PasswordChangeEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PasswordChangeEventConsumer {
+    private final ObjectMapper objectMapper;
+    private final LogoutMemberUseCase logoutMemberUseCase;
+
+    @KafkaListener(topics = "member-change-password")
+    public void logout(String kafkaMessage) {
+        log.info("Kafka Message ->: {}", kafkaMessage);
+
+        try {
+            PasswordChangeEvent event = objectMapper.readValue(kafkaMessage, PasswordChangeEvent.class);
+            logoutMemberUseCase.logout(event.email());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
